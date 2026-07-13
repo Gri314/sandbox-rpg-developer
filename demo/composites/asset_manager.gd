fa90d668bfb7f0c45ce8d768ff52d87536ce4611
Kdog314 <eshirley2000@gmail.com>
2026-07-13 13:38:02 -0700

    Added some window functionality.

--- demo/composites/asset_manager.gd
+++ demo/composites/asset_manager.gd
@@ -6,8 +6,8 @@ class_name AssetManager extends Window
 ## Implementation of a history system
 ## also used for navigating file structure
 @onready var hist = {
-	"time" = 0,
-	"list" = ["res://"]
+	"time" = -1,
+	"list" = []
 }
 
 ## Necessary for simple logic
@@ -27,11 +27,9 @@ func _on_close_requested():
 ## History data structure operations
 func new_hist(path):
 	hist["list"].append(path)
-	hist["time"] = len(hist["list"])
-func pop_hist():
-	hist.pop()
-func load_from_hist():
-	pass
+	hist["time"] += 1
+func load_from_hist(stage) -> String:
+	return hist["list"][stage]
 
 
 
@@ -41,13 +39,6 @@ func show_error(text: String):
 	error_disp.visible = true
 	error_disp.text = text
 
-## Folders get opened and searched
-func attempt_open_directory(dir):
-	print(dir)
-## Files are sent back to the target (like an upload)
-func attempt_open_file(file):
-	print(file)
-
 ## Populate the list with item instances of the things found in the folder
 func populate_list(dirs = PackedStringArray([]), files = PackedStringArray([])):
 	for dir in dirs:
@@ -55,7 +46,7 @@ func populate_list(dirs = PackedStringArray([]), files = PackedStringArray([])):
 		current.set_text("📁 " + dir, dir)
 		current.type = "dir"
 		listy.add_child(current)
-		current.request_signal_connect(attempt_open_directory)
+		current.request_signal_connect(_on_opened_dir_button_pressed)
 	for file in files:
 		if ".import" not in file:
 			var current = item_inst.instantiate()
@@ -68,7 +59,7 @@ func depopulate_list():
 	## remove all from queue, except error msg (slot 0)
 	for child in listy.get_children().slice(1):
 		if child.type == "dir":
-			child.request_signal_disconnect(attempt_open_directory)
+			child.request_signal_disconnect(_on_opened_dir_button_pressed)
 		else:
 			child.request_signal_disconnect(attempt_open_file) 
 		child.queue_free() ## delete from queue
@@ -94,5 +85,28 @@ func _on_repopulate_pressed():
 		show_error("Filepath empty.")
 	
 	populate_list(dirs, files)
-	
-	
+
+
+
+## Folders get opened and searched
+## Called by: Submitted text field in `path` node
+func attempt_open_directory(dir):
+	new_hist(dir)
+	print(self.hist)
+	path_ind.text = dir
+	_on_repopulate_pressed()
+
+## When pressing folder button, we need to get the current directory
+## called by: pressing "open folder"
+func _on_opened_dir_button_pressed(dir):
+	var newdir = hist["list"][hist["time"]] + "/" + dir
+	attempt_open_directory(newdir)
+
+## Files are sent back to the target (like an upload, not yet set up)
+func attempt_open_file(file):
+	print(file)
+
+
+## Use the history to go backwards and forwards
+func _on_back_pressed() -> void:
+	attempt_open_directory(load_from_hist(hist["time"]-1))
